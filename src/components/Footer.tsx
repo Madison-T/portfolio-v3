7f7f3e031573047ce2ba0522cd4f5472a6c550f7
Madison Tana <madisonlilah@gmail.com>
2026-04-16 11:59:33 +1200

    add link to previous portfolio

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,14 @@ const Footer = () => {
             <p className="text-sm text-brand-muted">
                 © {new Date().getFullYear()} Madison. Designed & Built with Precision.
             </p>
+            <a
+                href="https://madison-t.github.io/my-portfolio-v2"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-xs text-brand-muted hover:text-brand-accent trasition-colors duration-300"
+            >
+                View previous portfoio →
+                </a>
         </footer>
     )
 }
